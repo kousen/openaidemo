7d0a4f4243d3f6abb609b68d26fc70033028924a
Ken Kousen <ken.kousen@kousenit.com>
2023-09-16 16:43:54 -0400

    Refactored wav file handling and transcription process
    Updated the implementation of the WhisperAI and WavFileSplitter classes to enhance the handling of wav files. Improved the transcription process to accommodate large files by splitting them into chunks before transcribing, in accordance with the maximum chunk size specified by the OpenAI API. Introduced a new method to transcribe each chunk processed by the WavFileSplitter. Modified the test cases to incorporate these changes and verify that the chunks are properly transcribed.
    
    This change ensures that the application can process larger files without exceeding the API's maximum file size restrictions, expanding its usability and functionality.

--- src/main/java/com/kousenit/openai/WavFileSplitter.java
+++ src/main/java/com/kousenit/openai/WavFileSplitter.java
@@ -1,31 +1,26 @@
 package com.kousenit.openai;
 
 import javax.sound.sampled.*;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WavFileSplitter {
+    // maximum chunk size of 25MB; use 20MB to be safe
     private static final long MAX_CHUNK_SIZE_BYTES = 20 * 1024 * 1024;
 
-    public List<byte[]> splitWavFileIntoChunks(File sourceWavFile)
-            throws UnsupportedAudioFileException, IOException {
+    public List<File> splitWavFileIntoChunks(File sourceWavFile) {
+        List<File> chunks = new ArrayList<>();
+        int chunkCounter = 1;
 
-        // maximum chunk size of 25MB; use 20MB to be safe
-
-        AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(sourceWavFile);
-        AudioFormat format = fileFormat.getFormat();
-
-        // Calculate the maximum number of frames for each chunk
-        int frameSize = format.getFrameSize(); // Number of bytes in each frame
-        long framesPerChunk = MAX_CHUNK_SIZE_BYTES / frameSize;
-
-        List<byte[]> chunks = new ArrayList<>();
         try (var inputStream = AudioSystem.getAudioInputStream(sourceWavFile)) {
             long totalFrames = inputStream.getFrameLength(); // Total frames in the source wav file
+            AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(sourceWavFile);
+            AudioFormat format = fileFormat.getFormat();
+
+            // Calculate the maximum number of frames for each chunk
+            int frameSize = format.getFrameSize(); // Number of bytes in each frame
+            long framesPerChunk = MAX_CHUNK_SIZE_BYTES / frameSize;
 
             byte[] buffer = new byte[(int) (framesPerChunk * frameSize)];
 
@@ -33,19 +28,23 @@ public class WavFileSplitter {
                 long framesInThisFile = Math.min(totalFrames, framesPerChunk);
                 int bytesRead = inputStream.read(buffer, 0, (int) (framesInThisFile * frameSize));
                 if (bytesRead > 0) {
-                    try (var baos = new ByteArrayOutputStream()) {
-                        try (var partStream = new AudioInputStream(
-                                new ByteArrayInputStream(buffer, 0, bytesRead),
-                                format,
-                                framesInThisFile)) {
-                            AudioSystem.write(partStream, AudioFileFormat.Type.WAVE, baos);
-                        }
-                        chunks.add(baos.toByteArray());
+                    File chunkFile = new File(sourceWavFile.getAbsolutePath().replace(
+                            ".wav", "-" + chunkCounter + ".wav"));
+                    try (var partStream = new AudioInputStream(
+                            new ByteArrayInputStream(buffer, 0, bytesRead),
+                            format,
+                            framesInThisFile)) {
+                        AudioSystem.write(partStream, AudioFileFormat.Type.WAVE, chunkFile);
                     }
+                    chunks.add(chunkFile);
+                    chunkCounter++;
                 }
                 totalFrames -= framesInThisFile;
             }
+        } catch (UnsupportedAudioFileException | IOException e) {
+            throw new RuntimeException(e);
         }
-        return chunks; // returns the chunks as byte[]
+
+        return chunks;
     }
 }
--- src/test/java/com/kousenit/openai/WavFileSplitterTest.java
+++ src/test/java/com/kousenit/openai/WavFileSplitterTest.java
@@ -2,9 +2,10 @@ package com.kousenit.openai;
 
 import org.junit.jupiter.api.Test;
 
-import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,12 +20,17 @@ class WavFileSplitterTest {
         assertThat(sourceWavFile.length()).isGreaterThan(25 * 1024 * 1024);
 
         WavFileSplitter splitter = new WavFileSplitter();
-        try {
-            List<byte[]> chunks = splitter.splitWavFileIntoChunks(sourceWavFile);
-            chunks.forEach(chunk ->
-                    assertThat(chunk.length).isLessThanOrEqualTo(20 * 1024 * 1024));
-        } catch (UnsupportedAudioFileException | IOException ex) {
-            throw new RuntimeException(ex);
+        List<File> chunks = splitter.splitWavFileIntoChunks(sourceWavFile);
+        for (int i = 0; i < chunks.size(); i++) {
+            File chunk = chunks.get(i);
+            assertThat(chunk.length()).isLessThanOrEqualTo(25 * 1024 * 1024);
+            System.out.println("File: " + chunk.getName() + ", Chunk size: " + chunk.length());
+            String outputFileName = String.format("%s/chunk_%d.wav", "src/main/resources", i);
+            try (FileOutputStream fos = new FileOutputStream(outputFileName)) {
+                fos.write(Files.readAllBytes(chunk.toPath()));
+            } catch (IOException e) {
+                System.err.println("Failed to write chunk to file: " + e.getMessage());
+            }
         }
     }
 
