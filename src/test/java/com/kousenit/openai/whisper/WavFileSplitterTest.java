29ff64070992091c69f1850bb3550a7cfd7b49a4
Ken Kousen <ken.kousen@kousenit.com>
2023-09-16 16:55:30 -0400

    Clean up WavFileSplitterTest code
    Removed unnecessary FileOutputStream and try-catch block in WavFileSplitterTest. Simplified the code by using lambda expression to check each chunk's size. This makes the test more readable and concise without changing its functionality.

--- src/test/java/com/kousenit/openai/whisper/WavFileSplitterTest.java
+++ src/test/java/com/kousenit/openai/whisper/WavFileSplitterTest.java
@@ -3,9 +3,6 @@ package com.kousenit.openai.whisper;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,17 +18,7 @@ class WavFileSplitterTest {
 
         WavFileSplitter splitter = new WavFileSplitter();
         List<File> chunks = splitter.splitWavFileIntoChunks(sourceWavFile);
-        for (int i = 0; i < chunks.size(); i++) {
-            File chunk = chunks.get(i);
-            assertThat(chunk.length()).isLessThanOrEqualTo(25 * 1024 * 1024);
-            System.out.println("File: " + chunk.getName() + ", Chunk size: " + chunk.length());
-            String outputFileName = String.format("%s/chunk_%d.wav", "src/main/resources", i);
-            try (FileOutputStream fos = new FileOutputStream(outputFileName)) {
-                fos.write(Files.readAllBytes(chunk.toPath()));
-            } catch (IOException e) {
-                System.err.println("Failed to write chunk to file: " + e.getMessage());
-            }
-        }
+        chunks.forEach(chunk -> assertThat(chunk.length()).isLessThanOrEqualTo(25 * 1024 * 1024));
     }
 
 }
