e55401c04ad153e33dcae5f5d272d5d1f67c7bcf
Ken Kousen <ken.kousen@kousenit.com>
2023-10-20 12:28:33 -0400

    Refactor directory paths to constants in FileUtils
    The literal directory paths used in FileUtils have been replaced with constant strings. This refactoring was done to make the code cleaner, more maintainable, and to avoid potential issues in the future if the paths need to be changed. Now, changing the directory path only needs to be done in one place, rather than multiple. Pairing directory paths with meaningful constant names also enhances readability.

--- src/main/java/com/kousenit/utilities/FileUtils.java
+++ src/main/java/com/kousenit/utilities/FileUtils.java
@@ -12,11 +12,13 @@ import java.util.Base64;
 
 public class FileUtils {
     private static int counter;
+    private static final String TEXT_DIRECTORY = "src/main/resources/text";
+    private static final String IMAGE_DIRECTORY = "src/main/resources/images";
 
     static {
         try {
-            Files.createDirectories(Paths.get("src/main/resources/text"));
-            Files.createDirectories(Paths.get("src/main/resources/images"));
+            Files.createDirectories(Paths.get(TEXT_DIRECTORY));
+            Files.createDirectories(Paths.get(IMAGE_DIRECTORY));
         } catch (IOException e) {
             throw new ExceptionInInitializerError("Error creating directory");
         }
@@ -33,12 +35,12 @@ public class FileUtils {
     public static boolean writeImageToFile(String imageData) {
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         String fileName = String.format("image_%s_%d.png", timestamp, counter++);
-        Path directory = Paths.get("src/main/resources/images");
+        Path directory = Paths.get(IMAGE_DIRECTORY);
         Path filePath = directory.resolve(fileName);
         try {
             byte[] bytes = Base64.getDecoder().decode(imageData);
             Files.write(filePath, bytes, StandardOpenOption.CREATE_NEW);
-            System.out.printf("Saved %s to src/main/resources/images%n", fileName);
+            System.out.printf("Saved %s to %s%n", fileName, IMAGE_DIRECTORY);
             return true;
         } catch (IOException e) {
             throw new UncheckedIOException("Error writing image to file", e);
@@ -48,11 +50,11 @@ public class FileUtils {
     public static boolean writeByteArrayToFile(byte[] bytes) {
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         String fileName = String.format("image_%s_%d.png", timestamp, counter++);
-        Path directory = Paths.get("src/main/resources/images");
+        Path directory = Paths.get(IMAGE_DIRECTORY);
         Path filePath = directory.resolve(fileName);
         try {
             Files.write(filePath, bytes, StandardOpenOption.CREATE_NEW);
-            System.out.printf("Saved %s to src/main/resources/images%n", fileName);
+            System.out.printf("Saved %s to %s%n", fileName, IMAGE_DIRECTORY);
             return true;
         } catch (IOException e) {
             throw new UncheckedIOException("Error writing image to file", e);
@@ -60,12 +62,12 @@ public class FileUtils {
     }
 
     public static void writeTextToFile(String textData, String fileName) {
-        Path directory = Paths.get("src/main/resources/text");
+        Path directory = Paths.get(TEXT_DIRECTORY);
         Path filePath = directory.resolve(fileName);
         try {
             Files.deleteIfExists(filePath);
             Files.writeString(filePath, textData, StandardOpenOption.CREATE_NEW);
-            System.out.printf("Saved %s to src/main/resources/text%n", fileName);
+            System.out.printf("Saved %s to %s%n", fileName, IMAGE_DIRECTORY);
         } catch (IOException e) {
             throw new UncheckedIOException("Error writing text to file", e);
         }
